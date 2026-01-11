6ea6129c0f4935f21a046b432f514b5a01f95cf2
Arya Gupta <aryagupta239@gmail.com>
2026-01-12 02:00:34 +0530

    remove workers from fastapi startup

--- app/main.py
+++ app/main.py
@@ -1,16 +1,9 @@
 from fastapi import FastAPI, Request, HTTPException
-import asyncio
-
 from app.schemas import RideRequest
 from app.rate_limiter import rate_limit
 from app.ride_queue import enqueue_ride
-from app.workers import start_workers
 
 app = FastAPI(title="RideFlow Backend")
-
-@app.on_event("startup")
-async def startup():
-    asyncio.create_task(start_workers(4))
 
 @app.post("/request-ride")
 async def request_ride(ride: RideRequest, request: Request):
