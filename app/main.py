b617146b383a5c369dc9e09f6cdb6f36ff4c2759
Arya Gupta <aryagupta239@gmail.com>
2026-01-12 01:29:54 +0530

    main file for execution

---
+++ app/main.py
@@ -0,0 +1,24 @@
+# app/main.py
+from fastapi import FastAPI, Request, HTTPException
+import asyncio
+
+from schemas import RideRequest
+from rate_limiter import rate_limit
+from queue import enqueue_ride
+from workers import start_workers
+
+app = FastAPI(title="RideFlow Backend")
+
+@app.on_event("startup")
+async def startup():
+    asyncio.create_task(start_workers(4))
+
+@app.post("/request-ride")
+async def request_ride(ride: RideRequest, request: Request):
+    try:
+        rate_limit(f"ride:{request.client.host}")
+    except Exception as e:
+        raise HTTPException(status_code=429, detail=str(e))
+
+    enqueue_ride(ride.dict())
+    return {"status": "Ride queued successfully"}
